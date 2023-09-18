cd03eb7433af1942cffe2d0340e6d9f1f9c04f3a
刘尧 <email@sample.com>
2023-09-18 12:46:04 +0800

    concat

--- Array/数组常见方法.js
+++ Array/数组常见方法.js
@@ -1,6 +1,6 @@
 // 1.在起始和末尾添加或删除元素 push/pop  unshift/shift
 
-const arr = ["b", "c", "d", "r", "g"]
+const arr = [1, 2, 3]
 /**
  * push和unshift会返回添加了新元素的数组长度，
  * pop 和 shift 返回删除的元素
@@ -11,7 +11,13 @@ arr.pop() //删除 g
 arr.unshift("v") //在数组起始位添加 v
 arr.shift() //删除 b
 
-// 2.在末尾添加多个元素 concat 
-/** 
- * 
+// 2.在末尾添加多个元素 concat
+/**
+ * concat 给数组添加多个元素并返回新数组的拷贝，
+ * 如果concat 中有多层数组会拆分第一层数组
  */
+
+arr.concat(4, 5, 6) //返回[1,2,3,4,5,6] ,arr没有变动
+arr.concat([4, 5, 6]) //返回[1,2,3,4,5,6] ,arr没有变动
+arr.concat([4, 5], 6) //返回[1,2,3,4,5,6] ,arr没有变动
+arr.concat([[4, 5], 6]) //返回[1,2,3,[4,5],6] ,arr没有变动
